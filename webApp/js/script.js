1f58743b191d1f3e8f822c759b8bebe6b2bd05af
raya <1074723686@qq.com>
2020-04-28 20:11:41 +0800

    c3_app_3

--- webApp/js/script.js
+++ webApp/js/script.js
@@ -2,7 +2,8 @@
 new IScroll(".home");
 
 var Data = null,
-    arr = ['yunqian', 'yunzhong', 'chanqian', 'chanhou','chengzhang','fangzhi'];
+    arr = ['yunqian', 'yunzhong', 'chanqian', 'chanhou','chengzhang','fangzhi'],
+    brr = ['孕前准备','孕中知识','产前知识','分娩产后','幼儿成长指标','幼儿常见病防止'];
 function bindEvent(){
     /*get data*/
     $.ajax({
@@ -33,13 +34,26 @@ function bindEvent(){
         }
         into($(this));
 
-        /*是否进入列表页*/
         if (that==="#list"){
+            /*进入列表页*/
             getId(id);
+        }else if (that==="#article"){
+            /*进入文章页面*/
+            getRender($(this));
         }
     })
 }
 bindEvent();
+
+/*详情页*/
+function getRender(el) {
+    var crr = el.attr("data-content").split("_");
+    var str="";
+    str = Data[crr[0]]["fenlei"][crr[1]];
+    $(".article-content").html(str.content);
+    $(".header-main").html(str.title);
+    new IScroll("#article");
+}
 
 /*get id*/
 function getId(id) {
@@ -51,7 +65,7 @@ function getLoad(id) {
     var category = Data[id]['fenlei'],
         str = '';
     $.each(category, function (ind,val) {
-        str+="<a href=''><img src='img/tu/"+ val.img +"' alt=''><p>"+ val.title +"</p></a>"
+        str+="<a href='#article' data-content='"+id+"_"+ind+"'><img src='img/tu/"+ val.img +"' alt=''><p>"+ val.title +"</p></a>"
     })
     $('#listIscroll').html(str);
     new IScroll(".list");
@@ -61,12 +75,21 @@ function getLoad(id) {
 function into(that){
     var href = that.attr("href");
     var title = that.attr("title");
+    var fav = $(".header-right");
+    var back = $("#back");
     if (href==="#list"){
-        $("#back").show().attr("href","#home");
+        back.show().attr("href","#home");
         $('.header-main').html(title);
+        fav.hide();
     }else if (href==="#home"){
-        $("#back").hide();
-        $('.header-main').html("Supernova");
+        back.hide();
+        $('.header-main').html("孕育宝典");
+    }else if (href==="#article"){
+        fav.show();
+        back.attr("href","#list");
+        var splits = that.attr("data-content").split("_")[0];
+        var index = $.inArray(splits,arr);
+        back.attr("title",brr[index]);
     }
 }
 
